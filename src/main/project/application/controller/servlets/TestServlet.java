be282fe03bf0d077be8a505736cd0a9d4da95310
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-09-11 19:40:18 +0300

    Initial commit

---
+++ src/main/project/application/controller/servlets/TestServlet.java
@@ -0,0 +1,4 @@
+package application.controller.servlets;
+
+public class TestServlet {
+}
---
+++ src/main/project/application/service/DataSource.java
@@ -0,0 +1,33 @@
+package application.service;
+
+import java.sql.*;
+
+public class DataSource {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.h2.Driver").newInstance();
+            Connection conn = DriverManager.getConnection("jdbc:h2:test", "sa", "");
+            Statement st = null;
+            st = conn.createStatement();
+            st.execute("INSERT INTO TEST VALUES(default,'HELLO')");
+            st.execute("INSERT INTO TEST(NAME) VALUES('JOHN')");
+            String name1 = "Jack";
+            String q = "insert into TEST(name) values(?)";
+            PreparedStatement st1 = null;
+
+            st1 = conn.prepareStatement(q);
+            st1.setString(1, name1);
+            st1.execute();
+
+            ResultSet result;
+            result = st.executeQuery("SELECT * FROM TEST");
+            while (result.next()) {
+                String name = result.getString("NAME");
+                System.out.println(result.getString("ID") + " " + name);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
